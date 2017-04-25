31aefde237f6658422400d2a1a0c46f28237658d
johnstudarus <john@jhlconsulting.com>
2017-04-24 19:40:04 -0700

    renamed network from demo to internal via

--- Terraform/int-net.sh
+++ Terraform/int-net.sh
@@ -1,14 +1,16 @@
 TENANT_NETWORK_CIDR="192.168.100.0/24"
 TENANT_NETWORK_GATEWAY="192.168.100.1"
+TENANT_NETWORK_NAME="internal"
+TENANT_ROUTER_NAME="internal-router"
 
-neutron net-create demo-net
+neutron net-create $TENANT_NETWORK_NAME
 
-neutron subnet-create demo-net $TENANT_NETWORK_CIDR \
-  --name demo-subnet \
+neutron subnet-create $TENANT_NETWORK_NAME $TENANT_NETWORK_CIDR \
+  --name $TENANT_NETWORK_NAME \
   --gateway $TENANT_NETWORK_GATEWAY
 
-neutron router-create demo-router
+neutron router-create $TENANT_ROUTER_NAME
 
-neutron router-interface-add demo-router demo-subnet
+neutron router-interface-add $TENANT_ROUTER_NAME $TENANT_NETWORK_NAME
 
-neutron router-gateway-set demo-router ext-net
+neutron router-gateway-set $TENANT_ROUTER_NAME ext-net
