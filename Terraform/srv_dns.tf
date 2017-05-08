ee78341db88fed21036de07f90a02d175a209fbc
John Studarus <john@jhlconsulting.com>
2017-05-08 01:00:24 +0000

    moved DNS into its own file so it can be turned on/off easily

---
+++ Terraform/srv_dns.tf
@@ -0,0 +1,15 @@
+# somewhat recent version is needed for DNSimple
+terraform {
+  required_version = ">= 0.9.3"
+}
+
+resource "dnsimple_record" "controller" {
+  count  = "${var.server_count}"
+
+  domain = "openstacksandiego.us"
+  name   = "${element(packet_device.controller.*.hostname, count.index)}"
+  value  = "${element(packet_device.controller.*.network.0.address, count.index)}"
+
+  type   = "A"
+  ttl    = 3600
+}
